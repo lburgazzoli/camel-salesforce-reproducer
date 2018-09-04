388b75bf60a26ead227c38caac877b2a6f0461ee
lburgazzoli <lburgazzoli@gmail.com>
2018-09-04 15:07:53 +0200

    reproducer

--- src/main/java/io/syndesis/example/Application.java
+++ src/main/java/io/syndesis/example/Application.java
@@ -1,5 +1,7 @@
 package io.syndesis.example;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +18,34 @@ public class Application {
     public RouteBuilder route() {
         return new RouteBuilder() {
             public void configure() throws Exception {
-                from("salesforce:syn_Lead_c?sObjectClass=io.syndesis.example.SalesforceIdentifier&sObjectQuery=SELECT+Id+FROM+Lead")
+                from("salesforce:syn_Lead_c?sObjectClass=io.syndesis.example.Application$Id&sObjectQuery=SELECT+Id+FROM+Lead")
                     .enrich("salesforce:getSObject?rawPayload=true&sObjectName=Lead")
                     .convertBodyTo(String.class)
                     .to("log:sf?showAll=true&multiline=true");
             }
         };
+    }
+
+    public static class Id {
+        @JsonProperty("Id")
+        private String id;
+
+        @JsonCreator
+        public Id(@JsonProperty("Id") final String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(final String id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return id;
+        }
     }
 }
